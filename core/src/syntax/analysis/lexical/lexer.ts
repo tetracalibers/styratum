a6cdaf73f51da69d0d1fc7abeaf79bd4fe3ff850
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-26 19:11:36 +0900

    [core] クラスを別ファイルに分離

--- core/src/syntax/analysis/lexical/lexer.ts
+++ core/src/syntax/analysis/lexical/lexer.ts
@@ -1,51 +1,5 @@
-import E from 'fp-ts/Either'
 import { pipe } from 'fp-ts/lib/function'
-import { match, P } from 'ts-pattern'
-import { isBoundaryChar, peelOffWrapString } from '../../../util/regexp'
-import { Address } from '../../classes/Address'
-import { Token, TokenDefinition } from '../../classes/Token'
-import { DocumentStream } from './DocumentStream'
+import { peelOffWrapString } from '../../../util/regexp'
+import { Token } from '../../classes/Token'
 
 class Recorder {}
-
-abstract class Tokenizer {
-  protected stream: DocumentStream
-  protected uri: string
-  protected accumulator: string[]
-
-  constructor(text: string, uri: string) {
-    this.stream = new DocumentStream(text)
-    this.uri = uri
-    this.accumulator = []
-  }
-}
-
-class SyrmTokenizer extends Tokenizer {
-  private openAngleBracket(startAddress: Address) {
-    const builder = TokenDefinition.Builder.uri(this.uri)
-      .firstLetter('<')
-      .start(startAddress)
-    const nextCharE = this.stream.peek()
-
-    if (E.isLeft(nextCharE)) {
-      return builder.kind('operator').build()
-    }
-
-    const nextChar = nextCharE.right
-    // prettier-ignore
-    return match(nextChar)
-      .with(P.when(c => isBoundaryChar(c as string)),() => {
-        return builder.kind('operator').build()
-      })
-      .with('/', () => {
-        this.stream.takeAndSkip()
-        return builder
-          .kind('punctuation.closeTagBegin')
-          .appendChar(nextChar)
-          .build()
-      })
-      .otherwise(() => {
-        return builder.kind('punctuation.openTagBegin').build()
-      })
-  }
-}
