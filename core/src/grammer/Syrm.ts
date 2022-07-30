f46ff0dfc0f930b9c90ed14a022ece4979c2bad2
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-30 11:15:59 +0900

    [core -> grammer] Cascade Region内のパーサーを途中まで実装

--- core/src/grammer/Syrm.ts
+++ core/src/grammer/Syrm.ts
@@ -28,7 +28,7 @@ const parseSyrmRegions = (code: string) => {
         code,
         open.source.endIdx + 1,
         close.source.startIdx - 1
-      )
+      ) as Region
       return [
         {
           type: 'CASCADE',
@@ -70,16 +70,318 @@ const parseSyrmCascadeInner = (code: string) => {
   const parser = {} as Parser
   parser.grammer = NS.default.SyrmCascade
   parser.semantics = parser.grammer.createSemantics()
-  parser.semantics.addOperation('blocks', {})
+  parser.semantics.addOperation('blocks', {
+    _terminal() {
+      return [this.sourceString]
+    },
+    _iter(this, ...children) {
+      return children.map(child => child.blocks())
+    },
+    kebabCase(_, __) {
+      return [this.sourceString]
+    },
+    jsIdentifier(_, __) {
+      return [this.sourceString]
+    },
+    numeralOnly(_) {
+      return [this.sourceString]
+    },
+    PseudoClass(_, __) {
+      return [this.sourceString]
+    },
+    Root: bs => bs.children.map(child => child.blocks()),
+    Block: bs => {
+      return bs.children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(bs.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    DeclarationsBlockWithPrefix: (p, b) => {
+      const children = [p, b]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    DeclarationsBlock: (ss, _, ds, __) => {
+      const children = [ss, ds]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    SelectorList: (s1, _, s2) => {
+      const children = [s1, s2]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    Selector: (first, rest) => {
+      const children = [first, rest]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    SelectorElem_edge: (c, s) => {
+      const children = [c, s]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    Declaration: (pname, _, pvalue, __) => {
+      const children = [pname, pvalue]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    propsFunc: (func, _, arg, __) => {
+      const children = [func, arg]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    PropertyValueFunc: (name, _, fml1, __, fml2, ___) => {
+      const children = [name, fml1, fml2]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    PseudoFunc_class: (p, _, a, __) => {
+      const children = [p, a]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    nth_inject: (n, plus, props) => {
+      const children = [n, plus, props]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    numeralWithUnit: (n, unit) => {
+      const children = [n, unit]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    Formula_recursive: (e1, o, e2) => {
+      const children = [e1, o, e2]
+      return children.map(child => {
+        const { contents, startIdx, endIdx } = child.source
+        const range = new Region(child.sourceString, startIdx, endIdx)
+        return {
+          type: child.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: child.blocks(),
+        }
+      })
+    },
+    truthyKeyword(_at, _word, _, val, __) {
+      const { contents, startIdx, endIdx } = this.source
+      const range = new Region(this.sourceString, startIdx, endIdx)
+      return [
+        {
+          type: this.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: val.blocks(),
+        },
+      ]
+    },
+    falsyKeyword(_at, _falsy, _, val, __) {
+      const { contents, startIdx, endIdx } = this.source
+      const range = new Region(this.sourceString, startIdx, endIdx)
+      return [
+        {
+          type: this.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: val.blocks(),
+        },
+      ]
+    },
+    existKeyword(_at, _exist, _, val, __) {
+      const { contents, startIdx, endIdx } = this.source
+      const range = new Region(this.sourceString, startIdx, endIdx)
+      return [
+        {
+          type: this.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+          children: val.blocks(),
+        },
+      ]
+    },
+    elseKeyword(_at, _else) {
+      const { contents, startIdx, endIdx } = this.source
+      const range = new Region(this.sourceString, startIdx, endIdx)
+      return [
+        {
+          type: this.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+        },
+      ]
+    },
+    collectionKeyword(_at, _collection) {
+      const { contents, startIdx, endIdx } = this.source
+      const range = new Region(this.sourceString, startIdx, endIdx)
+      return [
+        {
+          type: this.ctorName,
+          text: contents,
+          location: {
+            uri: '',
+            range: range.position,
+          },
+        },
+      ]
+    },
+  })
   const match = parser.grammer.match(code)
   return parser.semantics(match).blocks()
 }
 
 export const parseSyrm = (raw_syrm: string) => {
   const regions = parseSyrmRegions(raw_syrm)
-  dumpJson(regions)('tmp/syrmRegions.json')
-  return regions.map((region: Token) => {
-    if (region.type === 'CASCADE') return parseSyrmCascadeInner(region.text)
+  return regions.map((rs: Token[]) => {
+    const [region] = rs
+    if (region.type === 'CASCADE') {
+      region.children = parseSyrmCascadeInner(region.text)
+    }
     return region
   })
 }
--- core/src/index.ts
+++ core/src/index.ts
@@ -1,9 +1,10 @@
 import shell from 'shelljs'
 import { parseSyrm } from './grammer/Syrm'
+import { dumpJson } from './util/json'
 
 const { cat } = shell
 
 const sampleCode = cat('src/sample-code/sampleC.syrm').toString()
 
 const parseResult = parseSyrm(sampleCode)
-//console.log('🚀 ~ file: index.ts ~ line 9 ~ parseResult', parseResult)
+dumpJson(parseResult)('tmp/syrmParseResult.json')
