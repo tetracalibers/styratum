8dd8a88ca77ca4450a20b72ca30d1b036e31dfc3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-31 09:43:34 +0900

    [core -> grammer -> def] 余計な定義DeclarationListを削除

--- core/src/grammer/def/build/Syrm.ohm-bundle.d.ts
+++ core/src/grammer/def/build/Syrm.ohm-bundle.d.ts
@@ -143,8 +143,7 @@ export interface SyrmedCssInterfaceGrammar extends Grammar {
 }
 
 export interface DeclarationBlockActionDict<T> extends SyrmedCssInterfaceActionDict<T> {
-  DeclarationBlock?: (this: NonterminalNode, arg0: TerminalNode, arg1: NonterminalNode, arg2: TerminalNode) => T;
-  DeclarationList?: (this: NonterminalNode, arg0: IterationNode) => T;
+  DeclarationBlock?: (this: NonterminalNode, arg0: TerminalNode, arg1: IterationNode, arg2: TerminalNode) => T;
   Declaration?: (this: NonterminalNode, arg0: NonterminalNode, arg1: TerminalNode, arg2: IterationNode, arg3: TerminalNode) => T;
 }
 
