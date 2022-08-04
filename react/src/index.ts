f9451bf19449a4f327cedef96b312485e79fbc6f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-04 09:19:42 +0900

    [react] Stack.syrmのASTを生成

--- react/src/index.ts
+++ react/src/index.ts
@@ -3,11 +3,15 @@ import { parse } from '@babel/parser'
 import { CodeGenerator } from '@babel/generator'
 import { transformFileSync, transformSync } from '@babel/core'
 import { dumpJson } from '@syrm-dev/json-helper'
+import { parseSyrm } from '@syrm/core'
 import { dump } from './util/dump'
 
 const { cat, ShellString } = shell
 
+const syrmcode = cat('src/sample/Stack/Stack.syrm').toString()
 const tsxcode = cat('src/sample/Stack/Stack.tsx').toString()
+
+const syrmast = parseSyrm('build')(syrmcode)
 
 const js = transformFileSync('src/sample/Stack/Stack.tsx', {
   presets: [
@@ -28,6 +32,7 @@ const jsmap = js?.map
 //
 //const output = new CodeGenerator(ast, {}, jscode).generate().code
 
-dump(jscode as string)('tmp/jscode.js')
-dumpJson(jsast as object)('tmp/jsast.json')
-dumpJson(jsmap as object)('tmp/jsmap.json')
+dumpJson(syrmast)('src/sample/Stack/tmp/syrmast.json')
+dump(jscode as string)('src/sample/Stack/tmp/jscode.js')
+dumpJson(jsast as object)('src/sample/Stack/tmp/jsast.json')
+dumpJson(jsmap as object)('src/sample/Stack/tmp/jsmap.json')
