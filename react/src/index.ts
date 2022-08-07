346f3de46d6987775c3e4033578545b6c54597d8
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-07 22:05:49 +0900

    [react] @babel/traverseのtraverse is not a functionエラー回避コードを模索

--- react/src/index.ts
+++ react/src/index.ts
@@ -2,6 +2,8 @@ import shell from 'shelljs'
 import { parse } from '@babel/parser'
 import { CodeGenerator } from '@babel/generator'
 import { transformFileSync, transformSync } from '@babel/core'
+import _traverser from '@babel/traverse'
+import * as t from '@babel/types'
 import { dumpJson } from '@syrm-dev/json-helper'
 import { parseSyrm, AstNode } from '@syrm/core'
 import { dump } from './util/dump'
@@ -24,8 +26,35 @@ const js = transformFileSync('src/sample/Stack/Stack.tsx', {
 })
 
 const jscode = js?.code
-const jsast = js?.ast
+const jsast = js?.ast as t.Node
 const jsmap = js?.map
+
+/**
+  @bug https://github.com/babel/babel/issues/13855
+ */
+const traverser = _traverser as typeof _traverser & { default: unknown }
+const traverse = traverser.default as typeof _traverser
+
+traverse(jsast, {
+  enter(path) {
+    if (
+      t.callExpression(
+        {
+          type: 'Identifier',
+          name: '_jsxDEV',
+        },
+        [
+          {
+            type: 'Identifier',
+            name: 'Syrm',
+          },
+        ]
+      )
+    ) {
+      console.log(path.node)
+    }
+  },
+})
 
 //const ast = parse(jscode, {
 //  sourceType: 'module',
@@ -39,6 +68,7 @@ dumpJson(jsast as object)('src/sample/Stack/tmp/jsast.json')
 dumpJson(jsmap as object)('src/sample/Stack/tmp/jsmap.json')
 
 import { get } from 'spectacles-ts'
+import { Traverse } from 'fp-ts/lib/Traversable'
 
 interface SyrmBlockStorage {
   cascade: AstNode[]
